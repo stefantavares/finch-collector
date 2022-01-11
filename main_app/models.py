d583e4a64766b87520991f5257b2a578b134c8c1
Stefan Tavares <stefantavares@Stefans-MacBook-Pro-2.local>
2022-01-10 19:55:35 -0800

    Part 2 complete

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Finch(models.Model):
+    type = models.CharField(max_length=100)
+    population = models.CharField(max_length=100)
+    habitat = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'({self.id}) - {self.type}'
--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,6 @@
 from django.shortcuts import render
-# from .models import finches as finches
+from .models import Finch
 
-
-class Finch():
-    def __init__(self, type, population, habitat):
-        self.type = type
-        self.population = population
-        self.habitat = habitat
-
-
-finches = [
-    Finch('Evening Grosbeak', '3.4 million', 'Northern and montane forests'),
-    Finch('Pine Grosbeak', '4.4 million', 'Open boreal forest'),
-    Finch('Gray-crowned Rosy-Finch', '200,000', 'Alpine tundra'),
-    Finch('Black Rosy-Finch', '20,000', 'Alpine tundra'),
-    Finch('Brown-capped Rosy-Finch', '45,000', 'Alpine tundra'),
-    Finch('House Finch', '31 million', 'Generalist'),
-    Finch('Purple Finch', '5.9 million',
-          'Mixed northern, montane, and boreal forests'),
-]
 
 # Create your views here.
 
@@ -32,4 +14,10 @@ def about(request):
 
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {'finches': finches})
+
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {'finch': finch})
