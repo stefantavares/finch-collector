3ede4c2652d9227719ece91bfe45edb93a8c0d97
Stefan Tavares <stefantavares@Stefans-MacBook-Pro-2.local>
2022-01-10 19:06:24 -0800

    Finish part 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+# from .models import finches as finches
+
+
+class Finch():
+    def __init__(self, type, population, habitat):
+        self.type = type
+        self.population = population
+        self.habitat = habitat
+
+
+finches = [
+    Finch('Evening Grosbeak', '3.4 million', 'Northern and montane forests'),
+    Finch('Pine Grosbeak', '4.4 million', 'Open boreal forest'),
+    Finch('Gray-crowned Rosy-Finch', '200,000', 'Alpine tundra'),
+    Finch('Black Rosy-Finch', '20,000', 'Alpine tundra'),
+    Finch('Brown-capped Rosy-Finch', '45,000', 'Alpine tundra'),
+    Finch('House Finch', '31 million', 'Generalist'),
+    Finch('Purple Finch', '5.9 million',
+          'Mixed northern, montane, and boreal forests'),
+]
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {'finches': finches})
