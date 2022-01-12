23e4dd31e32a3956e24f797b62a256d196ee10cf
Stefan Tavares <stefantavares@Stefans-MacBook-Pro-2.local>
2022-01-11 16:21:50 -0800

    Add edit finch functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 
 
@@ -27,3 +27,13 @@ def finches_detail(request, finch_id):
 class FinchCreate(CreateView):
     model = Finch
     fields = '__all__'
+
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['population', 'habitat']
+
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches/'
